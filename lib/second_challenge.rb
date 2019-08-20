379ce4e2b9e04fa8a5a9efb54ad9c3d0d757ae4a
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-20 21:07:32 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriatch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }}
 
   
